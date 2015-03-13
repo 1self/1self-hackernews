467b170b7588a7c81832334d209527b354b456e1
Anil Wadghule <anildigital@gmail.com>
2015-03-13 12:24:44 +0530

    Show error if user enters invalid hackernews username

--- app.rb
+++ app.rb
@@ -2,13 +2,16 @@ require 'sinatra'
 require 'rest-client'
 require 'time'
 require 'hnrb'
+require 'sinatra/flash'
 require_relative './1self_hackernews'
 
 CALLBACK_BASE_URI = ENV['CALLBACK_BASE_URI'] || 'http://localhost:4567'
 API_BASE_URL = ENV['API_BASE_URL'] || 'http://localhost:5000'
 
+
 configure do
   enable :sessions, :logging
+  register Sinatra::Flash
   set :logging, true
   set :session_secret, 'dqgAkAzrmpjt6XVxEAxkk3HKpMJdZsrn'
   set :views, "#{File.dirname(__FILE__)}/views"
@@ -17,6 +20,7 @@ end
 
 get '/' do
   session['registrationToken'] = params[:token]
+  @message = flash[:notice]
   erb :index
 end
 
@@ -24,12 +28,17 @@ post '/accept' do
   hn_username = params[:hn_username]
   callback_url = "#{CALLBACK_BASE_URI}/sync?hn_username=#{hn_username}&latestSyncField={{latestSyncField}}&streamid={{streamid}}"
 
+  begin
+    karmas = HackerNews1SelfLib.fetch_karma(hn_username)
+    puts 'Fetched karmas'
+  rescue Exception => e
+    flash[:notice] = "Invalid Hacker News Username"
+    redirect back and return
+  end
+
   stream_resp = HackerNews1SelfLib.register_stream(hn_username, session['registrationToken'], callback_url)
   stream = JSON.parse(stream_resp)
   puts 'Registered stream'
-
-  karmas = HackerNews1SelfLib.fetch_karma(hn_username)
-  puts 'Fetched karmas'
 
   oneself_events = HackerNews1SelfLib.convert_to_1self_events(karmas)
   puts 'Converted to 1self events'
